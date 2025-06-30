811ea1f35d46a925e9b43fa7b24bbfbd4106051c
BakirovDanil <danil.bakiroff84@gmail.com>
2025-06-30 18:35:14 +0500

    обновил функцию авторизации

--- security/authorization.py
+++ security/authorization.py
@@ -5,6 +5,7 @@ from sqlmodel import select, or_
 from database.ConnectionDB import SessionDep
 from models.CharacterModels import Director
 from typing import Annotated
+from logger_inf.logger_inf import logger
 
 security = HTTPBasic()
 def verification_of_the_head_or_deputy(credentials: Annotated[HTTPBasicCredentials, Depends(security)],
@@ -18,6 +19,7 @@ def verification_of_the_head_or_deputy(credentials: Annotated[HTTPBasicCredentia
     director = session.exec(select(Director).where(Director.id == credentials.username,
                                                    or_(Director.post == "Заведующий", Director.post == "Заместитель"))).first()
     if director is None:
+        logger.warning(f"Failed login attempt: unknown user or wrong position ({credentials.username})")
         raise HTTPException(
             status_code = status.HTTP_401_UNAUTHORIZED,
             detail = "Некорректное имя пользователя или некорректная должность",
@@ -27,11 +29,13 @@ def verification_of_the_head_or_deputy(credentials: Annotated[HTTPBasicCredentia
         credentials.password.encode("utf8"), director.password.encode("utf8")
     )
     if not is_correct_password:
+        logger.warning(f"Failed login attempt: wrong password ({credentials.username})")
         raise HTTPException(
             status_code = status.HTTP_401_UNAUTHORIZED,
             detail = "Неверный пароль пользователя",
             headers={"WWW-Authenticate": "Basic"}
         )
+    logger.warning(f"Access was successfully obtained ({credentials.username})")
 
 def verification_of_the_director(credentials: Annotated[HTTPBasicCredentials, Depends(security)],
                          session: SessionDep):
@@ -43,6 +47,7 @@ def verification_of_the_director(credentials: Annotated[HTTPBasicCredentials, De
     """
     director = session.exec(select(Director).where(Director.id == credentials.username)).first()
     if director is None:
+        logger.warning("Попытка несанкционированного доступа")
         raise HTTPException(
             status_code = status.HTTP_401_UNAUTHORIZED,
             detail = "Некорректное имя пользователя",
@@ -52,6 +57,7 @@ def verification_of_the_director(credentials: Annotated[HTTPBasicCredentials, De
         credentials.password.encode("utf8"), director.password.encode("utf8")
     )
     if not is_correct_password:
+        logger.warning("Попытка несанкционированного доступа")
         raise HTTPException(
             status_code = status.HTTP_401_UNAUTHORIZED,
             detail = "Неверный пароль пользователя",
